2560fd49fca248066496127a1dc43e9987160d4e
Presley Reed III <presleyoreed3@gmail.com>
2018-02-07 15:17:22 -0800

    added a date picker keyboard to birthday

--- StreetCheck/StreetCheck/ViewController.swift
+++ StreetCheck/StreetCheck/ViewController.swift
@@ -11,11 +11,16 @@ import UIKit
 class ViewController: UIViewController, UIPickerViewDataSource, UIPickerViewDelegate{
    
     
+    //MARK: Declarations
+    @IBOutlet weak var birthdayField: UITextField!
     
     @IBOutlet weak var eyeSelectionButton: UIButton!
     @IBOutlet weak var viewPicker: UIPickerView!
-    let eyeColors = ["Brown", "Blue", "Green", "Hazel", "Grey", "Other"]
     
+    let eyeColors = ["Brown", "Blue", "Green", "Hazel", "Grey", "Other"]
+    let birthdayPicker = UIDatePicker()
+    
+    //MARK: Load
     override func viewDidLoad() {
         
         viewPicker.delegate = self
@@ -24,13 +29,35 @@ class ViewController: UIViewController, UIPickerViewDataSource, UIPickerViewDele
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         viewPicker.isHidden = true
+        createDatePicker()
     }
-    @IBAction func eyeSelectionPressed(_ sender: UIButton) {
+    
+    //MARK: Birthday
+    
+    func createDatePicker() {
+        //toolbar
+        let toolbar = UIToolbar()
+        toolbar.sizeToFit()
+        
+        //Done button for Toolbar
+        let done = UIBarButtonItem(barButtonSystemItem: .done, target: nil, action: nil)
+        toolbar.setItems([done], animated: false)
+        
+        birthdayField.inputAccessoryView = toolbar
+        birthdayField.inputView = birthdayPicker
+        
+        birthdayPicker.datePickerMode = .date
+    }
+    
+    //MARK: Eyes
+    @IBAction func eyeWasSelected(_ sender: UIButton) {
         if (viewPicker.isHidden){
             viewPicker.isHidden = false
             eyeSelectionButton.isHidden = true
         }
     }
+    
+    
     func numberOfComponents(in pickerView: UIPickerView) -> Int {
         return 1
     }

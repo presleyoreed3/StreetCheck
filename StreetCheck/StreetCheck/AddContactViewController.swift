0100ecfbdde95861c834207a3c7450a45547ee3e
Presley Reed III <presleyoreed3@gmail.com>
2018-02-13 12:05:34 -0800

    Added declarations for necissary view controller elements

--- StreetCheck/StreetCheck/AddContactViewController.swift
+++ StreetCheck/StreetCheck/AddContactViewController.swift
@@ -14,14 +14,37 @@ UIImagePickerControllerDelegate, UINavigationControllerDelegate {
    
     
     //MARK: Declarations
+    @IBOutlet weak var fNameField: UITextField!
+    @IBOutlet weak var lNameField: UITextField!
+    @IBOutlet weak var aliasField: UITextField!
     @IBOutlet weak var birthdayField: UITextField!
     @IBOutlet weak var photoWindow: UIImageView!
     @IBOutlet weak var addPhotoButton: UIButton!
     @IBOutlet weak var eyeColorField: UITextField!
     @IBOutlet weak var hairColorField: UITextField!
     @IBOutlet weak var sexField: UITextField!
+    @IBOutlet weak var heightField: UITextField!
+    @IBOutlet weak var weightField: UITextField!
     
+    //MARK: Navigation
     @IBOutlet weak var saveButton: UIBarButtonItem!
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?){
+        guard let button = sender as? UIBarButtonItem, button === saveButton else{
+            os_log("The save button was not pressed, cancelling.", log: OSLog.default, type: .debug)
+            return
+        }
+        
+        let f_name = fNameField.text
+        let l_name = lNameField.text
+        let alias = aliasField.text
+        let birthday = birthday.date
+        let sex = sexField.text
+        let eye = eyeColorField.text
+        let hair = hairColorField.text
+        let height = heightField.text
+        
+        super.prepare(for: segue, sender: sender)
+    }
     
     //MARK: Load
     override func viewDidLoad() {

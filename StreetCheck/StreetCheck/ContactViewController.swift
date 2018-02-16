39366261a2dd2d81e3e83fb81e550f62ea2152c3
Presley Reed III <presleyoreed3@gmail.com>
2018-02-16 10:37:11 -0800

    Added a defualt contact page and controller

--- StreetCheck/StreetCheck/ContactViewController.swift
+++ StreetCheck/StreetCheck/ContactViewController.swift
@@ -1,167 +1,25 @@
 //
-//  ViewController.swift
+//  ContactViewController.swift
 //  StreetCheck
 //
-//  Created by Presley Reed III on 2/2/18.
+//  Created by Presley Reed III on 2/16/18.
 //  Copyright © 2018 Presley Reed III. All rights reserved.
 //
 
 import UIKit
-import os.log
 
-class ViewController: UIViewController, UIPickerViewDelegate, UIPickerViewDataSource,
-UIImagePickerControllerDelegate, UINavigationControllerDelegate {
-   
-    
-    //MARK: Declarations
-    @IBOutlet weak var fNameField: UITextField!
-    @IBOutlet weak var mNameField: UITextField!
-    @IBOutlet weak var lNameField: UITextField!
-    @IBOutlet weak var aliasField: UITextField!
-    @IBOutlet weak var birthdayField: UITextField!
-    @IBOutlet weak var photoWindow: UIImageView!
-    @IBOutlet weak var addPhotoButton: UIButton!
-    @IBOutlet weak var eyeColorField: UITextField!
-    @IBOutlet weak var hairColorField: UITextField!
-    @IBOutlet weak var sexField: UITextField!
-    @IBOutlet weak var heightField: UITextField!
-    @IBOutlet weak var weightField: UITextField!
-    
-    //MARK: Load
+class ContactViewController: UIViewController {
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        //Draws the birthday field
-        createDatePicker()
-        
-        // Draws the sex field
-        sexPicker.delegate = self
-        sexPicker.dataSource = self
-        sexField.inputView = sexPicker
-    }
-    
-    //MARK: Navigation
-    @IBAction func cancel(_ sender: UIBarButtonItem) {
-        dismiss(animated: true, completion: nil)
-    }
-    
-    // Contact to add to file
-    var contact: Contact?
-    
-    // Save button that returns to home screen
-    @IBOutlet weak var saveButton: UIBarButtonItem!
-     override func prepare(for segue: UIStoryboardSegue, sender: Any?){
-        
-        super.prepare(for: segue, sender: sender)
-        
-        guard let button = sender as? UIBarButtonItem, button === saveButton else {
-            os_log("The save button was not pressed, cancelling", log: OSLog.default, type: .debug)
-            return
-        }
-        
-        let f_name = fNameField.text
-        let m_name = mNameField.text
-        let l_name = lNameField.text
-        let alias = aliasField?.text
-        let birthday = birthdayField.text
-        let sex = sexField.text
-        let eye = eyeColorField?.text
-        let hair = hairColorField?.text
-        let height = heightField?.text
-        let weight = weightField?.text
-        let photo = photoWindow?.image
-        
 
-        contact = Contact(first_name: f_name!,middle_name: m_name, last_name: l_name!, alias: alias, birthday: birthday!, MO: nil, height: height!, weight: weight!, hair_color: hair!, eye_color: eye!, sex: sex!, address: nil, time_left: nil, photo: photo!)
-        
+        // Do any additional setup after loading the view.
     }
     
-    
-    //MARK: Birthday
-    
-    // Birthday Picker
-    let birthdayPicker = UIDatePicker()
-    
-    func createDatePicker() {
-        //toolbar
-        let toolbar = UIToolbar()
-        toolbar.sizeToFit()
-        
-        //Done button for Toolbar
-        let done = UIBarButtonItem(barButtonSystemItem: .done, target: nil, action: #selector(donePressed))
-        toolbar.setItems([done], animated: false)
-        
-        birthdayField.inputAccessoryView = toolbar
-        birthdayField.inputView = birthdayPicker
-        
-        birthdayPicker.datePickerMode = .date
-    }
-    
-    @objc func donePressed(){
-        //Format Date
-        let formatter = DateFormatter()
-        formatter.dateStyle = .medium
-        formatter.timeStyle = .none
-        let dateString = formatter.string(from: birthdayPicker.date)
-        
-        birthdayField.text = "\(dateString)"
-        self.view.endEditing(true)
-    }
-    
-    //MARK: Sex
-    
-    // Sex Array
-    let sexes = ["Male", "Female", "Other"]
-    var sexPicker = UIPickerView()
-    
-    func numberOfComponents(in sexPiceker: UIPickerView) -> Int {
-        return 1
-    }
-    
-    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
-        return sexes.count
-    }
-    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
-        return sexes[row]
-    }
-    
-    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
-        sexField.text = sexes[row]
-        sexField.resignFirstResponder()
-    }
-    
-    //MARK: Eye Color
-    let eyeColors = ["Brown", "Blue", "Green", "Hazel", "Grey", "Other"]
-    
-    
-    
-    //MARK: Image
-    @IBAction func addPhotoButton(_ sender: UIButton) {
-        //Resigns keyboard
-        birthdayField.resignFirstResponder()
-        
-        //Creates new image controller
-        let imagePickerController = UIImagePickerController()
-        
-        imagePickerController.sourceType = .photoLibrary
-        imagePickerController.delegate = self
-        
-        present(imagePickerController, animated: true, completion: nil)
-        
-    }
-    
-    func imagePickerControllerDidCancel(_ picker: UIImagePickerController){
-        dismiss(animated: true, completion: nil)
-    }
-    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]){
-        guard let selectedImage = info[UIImagePickerControllerOriginalImage] as? UIImage else {
-            fatalError("Expected a dictionary containing an image, but was provided the following: \(info)")
-        }
-        photoWindow.image = selectedImage
+    // MARK: Navigation
+    @IBAction func doneViewing(_ sender: UIBarButtonItem) {
         dismiss(animated: true, completion: nil)
     }
     
-    //Mark: Private Methods
     
 }
-

290aecf3d21d62720041fcba00d143bb1ec94d92
Presley Reed III <presleyoreed3@gmail.com>
2018-02-21 10:10:05 -0800

    Fixed issue with View Controller displaying info twice

--- StreetCheck/StreetCheck/ContactViewController.swift
+++ StreetCheck/StreetCheck/ContactViewController.swift
@@ -9,17 +9,32 @@
 import UIKit
 
 class ContactViewController: UIViewController {
+    
+    //MARK: Declarations
+    @IBOutlet weak var photoView: UIImageView!
+    @IBOutlet weak var f_nameLabel: UILabel!
+    @IBOutlet weak var m_nameLabel: UILabel!
+    @IBOutlet weak var l_nameLabel: UILabel!
+    
+    var contactOnDisplay: Contact?
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        print(contactOnDisplay)
         // Do any additional setup after loading the view.
+        f_nameLabel.text = contactOnDisplay?.first_name
+        m_nameLabel.text = contactOnDisplay?.middle_name
+        l_nameLabel.text = contactOnDisplay?.last_name
+        photoView.image = contactOnDisplay?.photo
     }
     
     // MARK: Navigation
-    @IBAction func doneViewing(_ sender: UIBarButtonItem) {
+    @IBAction func closeDetailView(_ sender: UIBarButtonItem) {
+        print("I was pressed")
         dismiss(animated: true, completion: nil)
     }
+    
     
     
 }

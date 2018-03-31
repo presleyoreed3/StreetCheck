9464fa8ebf399a3a8b2a1bbb5f6203b0a957c69f
Presley Reed III <presleyoreed3@gmail.com>
2018-03-31 13:11:47 -0700

    Added delete functionality to the Groups tab

--- StreetCheck/StreetCheck/Group/GroupsTableViewController.swift
+++ StreetCheck/StreetCheck/Group/GroupsTableViewController.swift
@@ -33,6 +33,23 @@ class GroupsTableViewController: UITableViewController {
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return groups.count
     }
+    
+    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
+        if editingStyle == .delete {
+            // Delete the row from the data source
+            groups.remove(at: indexPath.row)
+            saveGroup()
+            tableView.deleteRows(at: [indexPath], with: .fade)
+        } else if editingStyle == .insert {
+            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
+        }
+    }
+    
+    // Override to support conditional editing of the table view.
+    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
+        // Return false if you do not want the specified item to be editable.
+        return true
+    }
 
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         

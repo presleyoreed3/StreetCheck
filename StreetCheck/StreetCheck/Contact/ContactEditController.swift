4da1b93a0db13ec696502d933bd7a0b4b0b86afb
Presley Reed III <presleyoreed3@gmail.com>
2018-04-01 14:37:15 -0700

    Enabled users to take photos or pick photos when editing/adding info

--- StreetCheck/StreetCheck/Contact/ContactEditController.swift
+++ StreetCheck/StreetCheck/Contact/ContactEditController.swift
@@ -164,21 +164,40 @@ class ContactEditController: UIViewController, UIPickerViewDelegate, UIPickerVie
     //MARK: Image
     @IBAction func addPhotoButton(_ sender: UIButton) {
         //Resigns keyboard
-        birthdayField.resignFirstResponder()
+        view.resignFirstResponder()
         
-        //Creates new image controller
         let imagePickerController = UIImagePickerController()
-        
-        imagePickerController.sourceType = .photoLibrary
         imagePickerController.delegate = self
         
-        present(imagePickerController, animated: true, completion: nil)
+        
+        let actionSheet = UIAlertController(title: "Photo Source", message: "Choose a sources", preferredStyle: .actionSheet)
+        
+        actionSheet.addAction(UIAlertAction(title: "Camera", style: .default, handler: {(action:UIAlertAction) in if UIImagePickerController.isSourceTypeAvailable(.camera){
+            imagePickerController.allowsEditing = false
+            imagePickerController.sourceType = .camera
+            self.present(imagePickerController, animated: true, completion: nil)
+        }else{
+            print("Camera not Availible")
+            }
+        }))
+        
+        actionSheet.addAction(UIAlertAction(title: "Photo Library", style: .default, handler: {(action:UIAlertAction) in
+            imagePickerController.allowsEditing = false
+            imagePickerController.sourceType = .photoLibrary
+            self.present(imagePickerController, animated: true, completion: nil)
+        }))
+        
+        actionSheet.addAction(UIAlertAction(title: "Cancel", style: .cancel, handler: nil))
+        
+        self.present(actionSheet, animated: true, completion: nil)
         
     }
+    
     
     func imagePickerControllerDidCancel(_ picker: UIImagePickerController){
         dismiss(animated: true, completion: nil)
     }
+    
     func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]){
         guard let selectedImage = info[UIImagePickerControllerOriginalImage] as? UIImage else {
             fatalError("Expected a dictionary containing an image, but was provided the following: \(info)")
@@ -186,6 +205,4 @@ class ContactEditController: UIViewController, UIPickerViewDelegate, UIPickerVie
         photoWindow.image = selectedImage
         dismiss(animated: true, completion: nil)
     }
-    
-
 }

6e9c25d90a94fbc5384aeab0bff55ba3b6e25042
Presley Reed III <presleyoreed3@gmail.com>
2018-04-08 09:28:04 -0700

    fixed update your info to reflect change to id

--- StreetCheck/StreetCheck/EditYourInfoViewController.swift
+++ StreetCheck/StreetCheck/EditYourInfoViewController.swift
@@ -11,16 +11,13 @@ import UIKit
 class EditYourInfoViewController: UIViewController {
 
     @IBOutlet weak var nameField: UITextField!
-    @IBOutlet weak var emailField: UITextField!
-    @IBOutlet weak var phoneField: UITextField!
     @IBOutlet weak var mismatchLabel: UILabel!
+    @IBOutlet weak var idField: UITextField!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         nameField.text = UserDefaults.standard.value(forKey: "name") as? String
-        emailField.text = UserDefaults.standard.value(forKey: "email") as? String
-        phoneField.text = UserDefaults.standard.value(forKey: "phone") as? String
-        
+        idField.text = UserDefaults.standard.value(forKey: "id") as? String
         mismatchLabel.text = ""
         // Do any additional setup after loading the view.
     }
@@ -28,8 +25,7 @@ class EditYourInfoViewController: UIViewController {
     
     @IBAction func updateWasPressed(_ sender: UIButton) {
         UserDefaults.standard.set(nameField.text, forKey: "name")
-        UserDefaults.standard.set(emailField.text, forKey: "email")
-        UserDefaults.standard.set(phoneField.text, forKey: "phone")
+        UserDefaults.standard.set(idField.text, forKey: "id")
         mismatchLabel.text = "Your information has been updated"
     }
     
